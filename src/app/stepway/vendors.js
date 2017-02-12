99e697b80dc83f7dd2f37ac7815a312cabbdb4b0
Masoud Mirzaei <mamos98@gmail.com>
2017-02-12 14:05:53 +0330

    Add formly-foundation templates to dependencies

--- src/app/stepway/vendors.js
+++ src/app/stepway/vendors.js
@@ -1,4 +1,4 @@
 import 'api-check'; // NOTE: always imported before angular-formly!
 import 'angular-formly';
-import 'angular-formly-templates-bootstrap';
+import 'angular-formly-templates-foundation';
 import 'angular-wizard';
